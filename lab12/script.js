efe0fccc5a6e5d4820e37905d901c71131d655c3
rebegley-su <rebegley@syr.edu>
2024-11-16 19:02:16 -0500

    Planets

---
+++ lab12/script.js
@@ -0,0 +1,59 @@
+function PlanetsOff() {
+    document.getElementById('mercury').style.display = 'none';
+    document.getElementById('venus').style.display = 'none';
+    document.getElementById('earth').style.display = 'none';
+    document.getElementById('mars').style.display = 'none';
+    document.getElementById('jupiter').style.display = 'none';
+    document.getElementById('saturn').style.display = 'none';
+    document.getElementById('uranus').style.display = 'none';
+    document.getElementById('neptune').style.display = 'none';
+
+    document.getElementById('mercuryinfo').style.display = 'none';
+    document.getElementById('venusinfo').style.display = 'none';
+    document.getElementById('earthinfo').style.display = 'none';
+    document.getElementById('marsinfo').style.display = 'none';
+    document.getElementById('jupiterinfo').style.display = 'none';
+    document.getElementById('saturninfo').style.display = 'none';
+    document.getElementById('uranusinfo').style.display = 'none';
+    document.getElementById('neptuneinfo').style.display = 'none';
+}
+
+function MercuryOn() {
+    document.getElementById('mercury').style.display = 'block';
+    document.getElementById('mercuryinfo').style.display = 'block';
+}
+
+function VenusOn() {
+    document.getElementById('venus').style.display = 'block';
+    document.getElementById('venusinfo').style.display = 'block';
+}
+
+function EarthOn() {
+    document.getElementById('earth').style.display = 'block';
+    document.getElementById('earthinfo').style.display = 'block';
+}
+
+function MarsOn() {
+    document.getElementById('mars').style.display = 'block';
+    document.getElementById('marsinfo').style.display = 'block';
+}
+
+function JupiterOn() {
+    document.getElementById('jupiter').style.display = 'block';
+    document.getElementById('jupiterinfo').style.display = 'block';
+}
+
+function SaturnOn() {
+    document.getElementById('saturn').style.display = 'block';
+    document.getElementById('saturninfo').style.display = 'block';
+}
+
+function UranusOn() {
+    document.getElementById('uranus').style.display = 'block';
+    document.getElementById('uranusinfo').style.display = 'block';
+}
+
+function NeptuneOn() {
+    document.getElementById('neptune').style.display = 'block';
+    document.getElementById('neptuneinfo').style.display = 'block';
+}
